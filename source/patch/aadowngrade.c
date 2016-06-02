3a68eed391c6b55cc295aa054f1b43161ae21f77
chaoskagami <chaos.kagami@gmail.com>
2016-06-02 06:50:22 -0400

    Since the patcher VM works now, rewrite known functional patches to be stubs (until we can dynamically load em)

--- source/patch/aadowngrade.c
+++ source/patch/aadowngrade.c
@@ -1,30 +1,8 @@
 #include "patch_file.h"
 
-/*
-   rel p9_exefs
-   find 6, 0x89, 0x0a, 0x81, 0x42, 0x02, 0xD2
-   fwd 5
-   set 1, 0xE0
- */
-
-extern exefs_h* firm_p9_exefs;
-
 PATCH(aadowngrade)
 {
-    uint8_t* firm_mem = (uint8_t*)firm_p9_exefs + sizeof(exefs_h) +
-                        firm_p9_exefs->fileHeaders[0].offset;
-    uint32_t size = firm_p9_exefs->fileHeaders[0].size;
+	execb(PATH_PATCHES "/aadowngrade.vco");
 
-	const uint8_t pattern[] = {0x89, 0x0A, 0x81, 0x42, 0x02, 0xD2};
-
-	uint8_t *off = memfind(firm_mem, size, pattern, 6);
-
-	if(off == NULL)
-		return 1; // Not found.
-
-	fprintf(stderr, "aadowngrade: %x\n", (uint32_t)off);
-
-	off[5] = 0xE0;
-
-	return 0;
+    return 0;
 }
--- source/patch/prot.c
+++ source/patch/prot.c
@@ -2,48 +2,9 @@
 
 // This patch applies the FIRM protection code needed for safe a9lh usage.
 
-/*
-  rel p9_exefs
-  find 4, "exe:"
-  back 0x1
-  back 0xff
-  find 4, 0x00, 0x28, 0x01, 0xDA
-  set 4, 0x00, 0x20, 0xC0, 0x46
- */
-
-extern exefs_h* firm_p9_exefs;
-
 PATCH(firmprot)
 {
-    uint8_t* firm_mem = (uint8_t*)firm_p9_exefs + sizeof(exefs_h) +
-                        firm_p9_exefs->fileHeaders[0].offset;
-    uint32_t size = firm_p9_exefs->fileHeaders[0].size;
-
-    // We look for 'exe:' first; this string is close to what we patch
-    uint8_t* off = memfind(firm_mem, size, (uint8_t*)"exe:", 4);
-
-    if (off == NULL) {
-        fprintf(stderr, "Couldn't find 'exe:' string.\n");
-        return 1;
-    }
-
-    fprintf(stderr, "Firmprot: 'exe:' string @ %x\n", (uint32_t)off);
-
-    uint8_t pattern[] = { 0x00, 0x28, 0x01, 0xDA };
-
-    uint8_t* firmprot = memfind(off - 0x100, 0x100, pattern, 4);
-
-    if (firmprot == NULL) {
-        fprintf(stderr, "Couldn't find firmprot code.\n");
-        return 2;
-    }
-
-    fprintf(stderr, "Firmprot: %x\n", (uint32_t)firmprot);
-
-    uint8_t patch[] = { 0x00, 0x20, 0xC0, 0x46 };
-    memcpy(firmprot, patch, 4);
-
-    fprintf(stderr, "Applied firmprot patch.\n");
+	execb(PATH_PATCHES "/prot.vco");
 
     return 0;
 }

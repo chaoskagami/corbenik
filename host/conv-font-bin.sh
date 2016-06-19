1f6077a94bdaf4d52a2251e56b10079e98d7ac59
chaoskagami <chaos.kagami@gmail.com>
2016-06-18 21:42:46 -0400

    Fixups for bdfe fork

--- host/conv-font-bin.sh
+++ host/conv-font-bin.sh
@@ -6,7 +6,7 @@ fi
 
 set -e
 
-./host/bdfe/bdfe native header verbose all external/tewi-font/tewi-medium-11.bdf > host/font.h
+./host/bdfe/bdfe -n -L -v -A external/tewi-font/tewi-medium-11.bdf > host/font.h
 grep "// Converted Font Size" host/font.h | sed -e 's|.* ||g' \
 	-e 's|^|const int font_width = |g' \
 	-e 's|x|;\nconst int font_height = |g' \

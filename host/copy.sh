becf50c146a25187a837ab37203eb4438fc3e2c2
chaoskagami <chaos.kagami@gmail.com>
2016-07-23 09:00:15 -0400

    Update a few bits

--- host/copy.sh
+++ host/copy.sh
@@ -11,7 +11,6 @@ mnt=/mnt/ext1
 mount -t vfat ${dev}1 $mnt || exit 0
 rm -rf $mnt/corbenik
 cp -r out/* $mnt/ || exit 0
-cp out/arm9loaderhax.bin $mnt/corbenik/boot/Corbenik || exit 0
 ( cd $mnt/ && ./n3ds_firm.sh )
 umount $mnt || exit 0
 sync || exit 0

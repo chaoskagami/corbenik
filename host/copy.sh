e8a15850382b0319ae5f8bccfffab28aa448d641
chaoskagami <chaos.kagami@gmail.com>
2016-09-27 00:59:43 -0400

    Update misc scripts

--- host/copy.sh
+++ host/copy.sh
@@ -7,9 +7,13 @@ mnt=/media
 dev=/dev/mmcblk0p
 
 mount -t vfat ${dev}1 $mnt || exit 0
+rm $mnt/arm9loaderhax{,_si}.bin
 rm -rf $mnt/corbenik
 cp -r out/* $mnt/ || exit 0
-( cd $mnt/ && ./n3ds_firm.sh )
-mv $mnt/arm9loaderhax{,_si}.bin
+if [ "$1" = "n" ]; then
+    ( cd $mnt/ && ./n3ds_firm.sh )
+else
+    ( cd $mnt/ && ./o3ds_firm.sh )
+fi
 umount $mnt || exit 0
 sync || exit 0
